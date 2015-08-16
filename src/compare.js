b74ba0d8294199d2212675e24211f7fe54696df5
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-16 18:42:54 +0200

    basic comparisonw

--- src/compare.js
+++ src/compare.js
@@ -2,7 +2,16 @@
   "use strict";
 
   function compare_debian_package_versions(v1, v2){
-    return 1
+    var _v1 = parseInt(v1)
+    var _v2 = parseInt(v2)
+    if (_v1 > _v2)
+        return 1
+    else {
+        if (_v1 < _v2)
+            return -1
+        else
+            return 0
+    }
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
--- test/test.js
+++ test/test.js
@@ -9,10 +9,22 @@ var assert = require("assert");
 describe('CompareVersionOfPackages', function() {
   describe('compare', function () {
 
-    it("always returns 1", function() {
-      var result = compare.compare_debian_package_versions("2", "3");
+    it("first parameter is greater than second one", function() {
+      var result = compare.compare_debian_package_versions("3", "2");
       assert.equal(1, result);
     });
+
+    it("first parameter is lower than second one", function() {
+      var result = compare.compare_debian_package_versions("2", "3");
+      assert.equal(-1, result);
+    });
+
+    it("first parameter is equal to second one", function() {
+      var result = compare.compare_debian_package_versions("2", "2");
+      assert.equal(0, result);
+    });
+
+
 
 
   });
