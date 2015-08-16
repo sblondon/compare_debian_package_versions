dd08858e85bf7bcdeacc98d81cf4b49c681ba3b7
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-16 19:50:16 +0200

    refacto: add constants

--- src/compare.js
+++ src/compare.js
@@ -2,6 +2,9 @@
   "use strict";
 
   function compare_debian_package_versions(v1, v2){
+    var GREATER = 1
+    var LOWER = -1
+    var EQUAL = 0
     var _v1 = v1.split(".")
     var _v2 = v2.split(".")
 
@@ -9,11 +12,11 @@
       var v1_number = parseInt(_v1[index])
       var v2_number = parseInt(_v2[index])
       if (v1_number > v2_number)
-          return 1
+          return GREATER
       if (v1_number < v2_number)
-          return -1
+          return LOWER
      }
-    return 0
+    return EQUAL
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
--- test/test.js
+++ test/test.js
@@ -8,35 +8,47 @@ var assert = require("assert");
 
 describe('CompareVersionOfPackages', function() {
   describe('basic comparison', function () {
+    var LOWER_VERSION = "2"
+    var GREATER_VERSION = "3"
+
     it("first parameter is greater than second one", function() {
-      var result = compare.compare_debian_package_versions("3", "2");
+      var result = compare.compare_debian_package_versions(
+                GREATER_VERSION, LOWER_VERSION);
       assert.equal(1, result);
     });
 
     it("first parameter is lower than second one", function() {
-      var result = compare.compare_debian_package_versions("2", "3");
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, GREATER_VERSION);
       assert.equal(-1, result);
     });
 
     it("first parameter is equal to second one", function() {
-      var result = compare.compare_debian_package_versions("2", "2");
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, LOWER_VERSION);
       assert.equal(0, result);
     });
   });
 
   describe('comparison with 3 points in version number', function () {
+    var LOWER_VERSION = "1.2.0"
+    var GREATER_VERSION = "1.3.0"
+
     it("first parameter is greater than second one", function() {
-      var result = compare.compare_debian_package_versions("1.3.0", "1.2.0");
+      var result = compare.compare_debian_package_versions(
+                GREATER_VERSION, LOWER_VERSION);
       assert.equal(1, result);
     });
 
     it("first parameter is lower than second one", function() {
-      var result = compare.compare_debian_package_versions("1.2.1", "1.3.1");
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, GREATER_VERSION);
       assert.equal(-1, result);
     });
 
     it("first parameter is equal to second one", function() {
-      var result = compare.compare_debian_package_versions("1.2.3", "1.2.3");
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, LOWER_VERSION);
       assert.equal(0, result);
     });
   });
