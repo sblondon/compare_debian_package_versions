176d5d8fa6ca15050944c0c0f6437987d7545b08
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-16 19:44:12 +0200

    compare with number with several points

--- src/compare.js
+++ src/compare.js
@@ -2,16 +2,18 @@
   "use strict";
 
   function compare_debian_package_versions(v1, v2){
-    var _v1 = parseInt(v1)
-    var _v2 = parseInt(v2)
-    if (_v1 > _v2)
-        return 1
-    else {
-        if (_v1 < _v2)
-            return -1
-        else
-            return 0
-    }
+    var _v1 = v1.split(".")
+    var _v2 = v2.split(".")
+
+    for(var index=0; index< _v1.length; index++){
+      var v1_number = parseInt(_v1[index])
+      var v2_number = parseInt(_v2[index])
+      if (v1_number > v2_number)
+          return 1
+      if (v1_number < v2_number)
+          return -1
+     }
+    return 0
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
--- test/test.js
+++ test/test.js
@@ -7,8 +7,7 @@ var assert = require("assert");
 
 
 describe('CompareVersionOfPackages', function() {
-  describe('compare', function () {
-
+  describe('basic comparison', function () {
     it("first parameter is greater than second one", function() {
       var result = compare.compare_debian_package_versions("3", "2");
       assert.equal(1, result);
@@ -23,10 +22,25 @@ describe('CompareVersionOfPackages', function() {
       var result = compare.compare_debian_package_versions("2", "2");
       assert.equal(0, result);
     });
-
-
-
-
   });
+
+  describe('comparison with 3 points in version number', function () {
+    it("first parameter is greater than second one", function() {
+      var result = compare.compare_debian_package_versions("1.3.0", "1.2.0");
+      assert.equal(1, result);
+    });
+
+    it("first parameter is lower than second one", function() {
+      var result = compare.compare_debian_package_versions("1.2.1", "1.3.1");
+      assert.equal(-1, result);
+    });
+
+    it("first parameter is equal to second one", function() {
+      var result = compare.compare_debian_package_versions("1.2.3", "1.2.3");
+      assert.equal(0, result);
+    });
+  });
+
+
 });
 
