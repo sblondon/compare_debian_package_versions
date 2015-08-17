5b70307f4d93d5826848cecd368b86095f784f55
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-17 11:17:52 +0200

    compare maintainer revision number

--- src/compare.js
+++ src/compare.js
@@ -1,22 +1,51 @@
 (function(exports) {
   "use strict";
 
-  function compare_debian_package_versions(v1, v2){
+  function compare_version_number(v1, v2){
     var GREATER = 1
     var LOWER = -1
     var EQUAL = 0
-    var _v1 = v1.split(".")
-    var _v2 = v2.split(".")
-
-    for(var index=0; index< _v1.length; index++){
-      var v1_number = parseInt(_v1[index])
-      var v2_number = parseInt(_v2[index])
+    for(var index=0; index< v1.length; index++){
+      var v1_number = parseInt(v1[index])
+      var v2_number = parseInt(v2[index])
       if (v1_number > v2_number)
           return GREATER
       if (v1_number < v2_number)
           return LOWER
      }
     return EQUAL
+  }
+
+  function compare_revision_number(v1, v2){
+    var GREATER = 1
+    var LOWER = -1
+    var EQUAL = 0
+    var v1_number = parseInt(v1)
+    var v2_number = parseInt(v2)
+    if (v1_number > v2_number)
+        return GREATER
+    if (v1_number < v2_number)
+        return LOWER
+    return EQUAL
+  }
+
+
+  function compare_debian_package_versions(v1, v2){
+    var GREATER = 1
+    var LOWER = -1
+    var EQUAL = 0
+    var _v1_version = v1.split("-")[0]
+    var _v1_revision = v1.split("-")[1]
+    var _v1 = _v1_version.split(".")
+    var _v2_version = v2.split("-")[0]
+    var _v2_revision = v2.split("-")[1]
+    var _v2 = _v2_version.split(".")
+
+    var version_number_comparision = compare_version_number(_v1, _v2)
+    if (version_number_comparision == EQUAL)
+        return compare_revision_number(_v1_revision, _v2_revision)
+    else
+        return version_number_comparision
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
--- test/test.js
+++ test/test.js
@@ -4,8 +4,6 @@ compare = require("../src/compare.js")
 var assert = require("assert");
 
 
-
-
 describe('CompareVersionOfPackages', function() {
   describe('basic comparison', function () {
     var LOWER_VERSION = "2"
@@ -33,6 +31,29 @@ describe('CompareVersionOfPackages', function() {
   describe('comparison with 3 points in version number', function () {
     var LOWER_VERSION = "1.2.0"
     var GREATER_VERSION = "1.3.0"
+
+    it("first parameter is greater than second one", function() {
+      var result = compare.compare_debian_package_versions(
+                GREATER_VERSION, LOWER_VERSION);
+      assert.equal(1, result);
+    });
+
+    it("first parameter is lower than second one", function() {
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, GREATER_VERSION);
+      assert.equal(-1, result);
+    });
+
+    it("first parameter is equal to second one", function() {
+      var result = compare.compare_debian_package_versions(
+                LOWER_VERSION, LOWER_VERSION);
+      assert.equal(0, result);
+    });
+  });
+
+  describe('comparison with debian maintainer revision version', function () {
+    var LOWER_VERSION = "1.3.0-1"
+    var GREATER_VERSION = "1.3.0-2"
 
     it("first parameter is greater than second one", function() {
       var result = compare.compare_debian_package_versions(
