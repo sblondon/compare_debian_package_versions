50f13a332b870bb9092fd680d1a2cb770209c827
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-17 11:22:26 +0200

    factorize constants

--- src/compare.js
+++ src/compare.js
@@ -1,10 +1,10 @@
 (function(exports) {
   "use strict";
-
+  var GREATER = 1
+  var LOWER = -1
+  var EQUAL = 0
+ 
   function compare_version_number(v1, v2){
-    var GREATER = 1
-    var LOWER = -1
-    var EQUAL = 0
     for(var index=0; index< v1.length; index++){
       var v1_number = parseInt(v1[index])
       var v2_number = parseInt(v2[index])
@@ -17,9 +17,6 @@
   }
 
   function compare_revision_number(v1, v2){
-    var GREATER = 1
-    var LOWER = -1
-    var EQUAL = 0
     var v1_number = parseInt(v1)
     var v2_number = parseInt(v2)
     if (v1_number > v2_number)
@@ -31,17 +28,12 @@
 
 
   function compare_debian_package_versions(v1, v2){
-    var GREATER = 1
-    var LOWER = -1
-    var EQUAL = 0
-    var _v1_version = v1.split("-")[0]
+    var _v1_version = v1.split("-")[0].split(".")
     var _v1_revision = v1.split("-")[1]
-    var _v1 = _v1_version.split(".")
-    var _v2_version = v2.split("-")[0]
+    var _v2_version = v2.split("-")[0].split(".")
     var _v2_revision = v2.split("-")[1]
-    var _v2 = _v2_version.split(".")
 
-    var version_number_comparision = compare_version_number(_v1, _v2)
+    var version_number_comparision = compare_version_number(_v1_version, _v2_version)
     if (version_number_comparision == EQUAL)
         return compare_revision_number(_v1_revision, _v2_revision)
     else
