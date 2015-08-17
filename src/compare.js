adbcf7e9ec2d5c883281c71c35a05c9941e467dc
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-17 19:25:29 +0200

    refactoring: reorder functions

--- src/compare.js
+++ src/compare.js
@@ -4,6 +4,24 @@
   var LOWER = -1
   var EQUAL = 0
 
+
+  function compare_debian_package_versions(v1, v2){
+    var _v1 = new Version(v1)
+    var _v2 = new Version(v2)
+
+    var epoch_number_comparision = compare_epoch_number(_v1, _v2)
+    if (epoch_number_comparision != EQUAL)
+        return epoch_number_comparision
+
+    var upstream_number_comparision = compare_upstream_version_numbers(_v1, _v2)
+    if (upstream_number_comparision == EQUAL){
+        return compare_revision_numbers(_v1, _v2)
+    }
+    else
+        return upstream_number_comparision
+  }
+
+
   function Version(raw_version){
     if(raw_version.indexOf(":") !== -1){
         this.epoch_number = parseInt(raw_version.split(":")[0])
@@ -31,6 +49,31 @@
     }
   }
 
+
+  function compare_epoch_number(v1, v2){
+    return compare_integer(v1.epoch_number, v2.epoch_number)
+  }
+
+
+  function compare_integer(i1, i2){
+    if (i1 > i2)
+        return GREATER
+    if (i1 < i2)
+        return LOWER
+    return EQUAL
+  }
+
+
+  function compare_upstream_version_numbers(v1, v2){
+    return compare_integer_lists(v1.upstream_numbers, v2.upstream_numbers)
+  }
+
+
+  function compare_revision_numbers(v1, v2){
+    return compare_integer_lists(v1.revision_numbers, v2.revision_numbers)
+  }
+
+
   function compare_integer_lists(l1, l2){
     var MAX_LENGTH = Math.max(l1.length, l2.length)
     for(var index=0; index < MAX_LENGTH; index++){
@@ -46,42 +89,6 @@
           return result
      }
     return EQUAL
-  }
-
-  function compare_upstream_version_numbers(v1, v2){
-    return compare_integer_lists(v1.upstream_numbers, v2.upstream_numbers)
-  }
-
-  function compare_revision_numbers(v1, v2){
-    return compare_integer_lists(v1.revision_numbers, v2.revision_numbers)
-  }
-
-  function compare_epoch_number(v1, v2){
-    return compare_integer(v1.epoch_number, v2.epoch_number)
-  }
-
-  function compare_integer(i1, i2){
-    if (i1 > i2)
-        return GREATER
-    if (i1 < i2)
-        return LOWER
-    return EQUAL
-  }
-
-  function compare_debian_package_versions(v1, v2){
-    var _v1 = new Version(v1)
-    var _v2 = new Version(v2)
-
-    var epoch_number_comparision = compare_epoch_number(_v1, _v2)
-    if (epoch_number_comparision != EQUAL)
-        return epoch_number_comparision
-
-    var upstream_number_comparision = compare_upstream_version_numbers(_v1, _v2)
-    if (upstream_number_comparision == EQUAL){
-        return compare_revision_numbers(_v1, _v2)
-    }
-    else
-        return upstream_number_comparision
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
