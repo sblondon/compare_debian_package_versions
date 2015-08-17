64c3686d8b1985f168f8f151ac688e6f16cbb758
Stephane Blondon <stephane.blondon@gmail.com>
2015-08-17 16:51:52 +0200

    refactoring: rename some 'version' words to 'upstream'

--- src/compare.js
+++ src/compare.js
@@ -14,20 +14,20 @@
         var without_epoch_number = raw_version
     }
 
-    var version_numbers = without_epoch_number.split("-")[0].split(".")
-    this.version_numbers = []
+    var upstream_numbers = without_epoch_number.split("-")[0].split(".")
+    this.upstream_numbers = []
     for(var index=0; index < without_epoch_number.length; index++){
-      this.version_numbers.push(parseInt(without_epoch_number[index]))
+      this.upstream_numbers.push(parseInt(without_epoch_number[index]))
     }
 
     this.revision_number = parseInt(without_epoch_number.split("-")[1]) || 0
   }
 
-  function compare_version_numbers(v1, v2){
-    var MAX_LENGTH = Math.max(v1.version_numbers.length, v2.version_numbers.length)
+  function compare_upstream_version_numbers(v1, v2){
+    var MAX_LENGTH = Math.max(v1.upstream_numbers.length, v2.upstream_numbers.length)
     for(var index=0; index < MAX_LENGTH; index++){
-      var v1_number = v1.version_numbers[index]
-      var v2_number = v2.version_numbers[index]
+      var v1_number = v1.upstream_numbers[index]
+      var v2_number = v2.upstream_numbers[index]
       if (v2_number === undefined || v1_number > v2_number)
           return GREATER
       if (v1_number === undefined || v1_number < v2_number)
@@ -60,11 +60,11 @@
     if (epoch_number_comparision != EQUAL)
         return epoch_number_comparision
 
-    var version_number_comparision = compare_version_numbers(_v1, _v2)
-    if (version_number_comparision == EQUAL)
+    var upstream_number_comparision = compare_upstream_version_numbers(_v1, _v2)
+    if (upstream_number_comparision == EQUAL)
         return compare_revision_number(_v1, _v2)
     else
-        return version_number_comparision
+        return upstream_number_comparision
   }
 
   exports.compare_debian_package_versions = compare_debian_package_versions;
